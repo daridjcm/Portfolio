ce2df8c0af91f1270f4c987131fd85a716cc061e
Daridjcm <daricorreamedina@gmail.com132844413+Daridjcm@users.noreply.github.com>
2024-01-09 11:11:25 -0500

    Add website
    Structure, style and functionality.

---
+++ linkScroll.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  $('.nav-link-scroll').on('click', function (event) {
+    event.preventDefault();
+    $('.nav-link-scroll').removeClass('active');
+    $(this).addClass('active');
+    var target = this.hash;
+    $('html, body').animate(
+      {
+        scrollTop: $(target).offset().top,
+      },
+      600,
+      'swing'
+    );
+  });
+  $(window).scroll(function () {
+    var scrollPos = $(document).scrollTop();
+    $('.section').each(function () {
+      var offset = $(this).offset().top;
+      var height = $(this).height();
+      var id = $(this).attr('id');
+      if (scrollPos >= offset && scrollPos < offset + height) {
+        $('.nav-link-scroll').removeClass('active');
+        $(".nav-link-scroll[href='#' + id + '']").addClass('active');
+      }
+    });
+  });
+});
---
+++ skillsOverlay.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const skills = document.querySelectorAll('.skill');
+
+  skills.forEach(function (skill) {
+    skill.addEventListener('click', function () {
+      const title = skill.getAttribute('data-title');
+      const description = skill.getAttribute('data-description');
+      const imageSrc = skill.querySelector('.skill-image').getAttribute('src');
+
+      const overlay = document.createElement('div');
+      overlay.classList.add('skill-overlay');
+
+      const overlayContent = document.createElement('div');
+      overlayContent.classList.add('skill-overlay-content');
+
+      const image = document.createElement('img');
+      image.src = imageSrc;
+
+      const titleElement = document.createElement('p');
+      titleElement.classList.add('lead');
+      titleElement.textContent = title;
+
+      const descriptionElement = document.createElement('p');
+      descriptionElement.classList.add('lead');
+      descriptionElement.textContent = description;
+
+      overlayContent.appendChild(image);
+      overlayContent.appendChild(titleElement);
+      overlayContent.appendChild(descriptionElement);
+
+      overlay.appendChild(overlayContent);
+      document.body.appendChild(overlay);
+
+      overlay.addEventListener('click', function () {
+        overlay.remove();
+      });
+    });
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const skillImages = document.querySelectorAll('.skill-image');
+
+  skillImages.forEach(function (image) {
+      image.addEventListener('click', function () {
+          skillImages.forEach(function (img) {
+              img.style.maxWidth = '100%';
+              img.style.maxHeight = '100%';
+              img.style.boxShadow = 'none';
+          });
+          image.style.maxWidth = '95%';
+          image.style.maxHeight = '100%';
+          image.style.boxShadow = '-0px 5px 15px #656565';
+      });
+  });
+});
