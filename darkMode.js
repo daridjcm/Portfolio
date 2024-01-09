31372a2f3f3da51eb36d49bfa10832d342460623
Daridjcm <daricorreamedina@gmail.com132844413+Daridjcm@users.noreply.github.com>
2024-01-09 11:17:17 -0500

    The double quotes have been changed to single quotes

--- darkMode.js
+++ darkMode.js
@@ -1,26 +1,26 @@
 window.onload = function () {
-  var darkModeBtn = document.getElementById("darkMode");
-  var bodyElement = document.querySelector("body");
-  var savedTheme = localStorage.getItem("theme");
+  var darkModeBtn = document.getElementById('darkMode');
+  var bodyElement = document.querySelector('body');
+  var savedTheme = localStorage.getItem('theme');
 
-  if (savedTheme === "dark") {
-    bodyElement.classList.add("dark-theme");
+  if (savedTheme === 'dark') {
+    bodyElement.classList.add('dark-theme');
     unloadDarkThemeStyles();
   } else {
     loadDarkThemeStyles();
   }
 
-  darkModeBtn.addEventListener("click", function () {
-    bodyElement.classList.toggle("dark-theme");
-    bodyElement.classList.toggle("dark-text");
-    bodyElement.classList.toggle("dark-background");
+  darkModeBtn.addEventListener('click', function () {
+    bodyElement.classList.toggle('dark-theme');
+    bodyElement.classList.toggle('dark-text');
+    bodyElement.classList.toggle('dark-background');
 
-    var currentTheme = bodyElement.classList.contains("dark-theme") ? "dark" : "light";
-    bodyElement.setAttribute("data-bs-theme", currentTheme);
+    var currentTheme = bodyElement.classList.contains('dark-theme') ? 'dark' : 'light';
+    bodyElement.setAttribute('data-bs-theme', currentTheme);
 
-    localStorage.setItem("theme", currentTheme === "dark" ? "dark" : "light");
+    localStorage.setItem('theme', currentTheme === 'dark' ? 'dark' : 'light');
 
-    if (currentTheme === "dark") {
+    if (currentTheme === 'dark') {
       loadDarkThemeStyles();
     } else {
       unloadDarkThemeStyles();
@@ -28,15 +28,15 @@ window.onload = function () {
   });
 
   function loadDarkThemeStyles() {
-    var linkElement = document.createElement("link");
-    linkElement.rel = "stylesheet";
-    linkElement.href = "darkTheme.css";
-    linkElement.id = "dark-theme-styles";
+    var linkElement = document.createElement('link');
+    linkElement.rel = 'stylesheet';
+    linkElement.href = 'darkTheme.css';
+    linkElement.id = 'dark-theme-styles';
     document.head.appendChild(linkElement);
   }
 
   function unloadDarkThemeStyles() {
-    var existingLink = document.getElementById("dark-theme-styles");
+    var existingLink = document.getElementById('dark-theme-styles');
     if (existingLink) {
       existingLink.parentNode.removeChild(existingLink);
     }
