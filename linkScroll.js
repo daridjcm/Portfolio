a86e1127842820254128a42fbcfea4366647a9e8
Daridjcm <daricorreamedina@gmail.com>
2024-01-28 13:05:11 -0500

    The double quotes have been changed to single quotes

--- linkScroll.js
+++ linkScroll.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
-  $(".nav-link-scroll").on("click", function(event) {
+  $('.nav-link-scroll').on('click', function(event) {
     event.preventDefault();
-    $(".nav-link-scroll").removeClass("active");
-    $(this).addClass("active");
+    $('.nav-link-scroll').removeClass('active');
+    $(this).addClass('active');
     var target = this.hash;
-    $("html, body").animate(
+    $('html, body').animate(
       {
         scrollTop: $(target).offset().top,
       },
       600,
-      "swing"
+      'swing'
     );
   });
   $(window).scroll(function() {
     var scrollPos = $(document).scrollTop();
-    $(".section").each(function() {
+    $('.section').each(function() {
       var offset = $(this).offset().top;
       var height = $(this).height();
-      var id = $(this).attr("id");
+      var id = $(this).attr('id');
       if (scrollPos >= offset && scrollPos < offset + height) {
-        $(".nav-link-scroll").removeClass("active");
-        $(".nav-link-scroll[href='#' + id + '']").addClass("active");
+        $('.nav-link-scroll').removeClass('active');
+        $(".nav-link-scroll[href='#' + id + '']").addClass('active');
       }
     });
   });
